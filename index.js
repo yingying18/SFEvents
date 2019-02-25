0f0aed6592dd08a622c303f7a15749673c96fc42
Eugene li <eugeneli71@gmail.com>
2019-02-24 21:17:12 -0800

    update server to listen AWS's port and ip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT  || 3000;
+const ip   = process.env.IP    || '0.0.0.0';
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/public/index.html')
@@ -27,4 +28,5 @@ app.get('/yujin', (req, res) => {
   }
 )
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port,ip)
+ console.log(`Example app listening on port ${port}!`)
