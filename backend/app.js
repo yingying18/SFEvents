a74e601b96a5ed229e183637341e479cc746f5f0
Tejal Ghadge <tejalghadge@Tejals-MacBook-Pro.local>
2019-03-25 14:46:12 -0700

    Search and Insert services for Milestone 2

---
+++ backend/app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var mysql = require('mysql');
+
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout:'main' });
+
+app.set('port', process.env.PORT || 8080);
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.listen(app.get('port'), function () {
+
+  console.log("Express started on http://localhost:"+app.get('port'));
+});
+
+/*
+ This function creates a connection to team2 database with admin user
+ */
+function createConnection() {
+  return mysql.createConnection({
+    host: "3.16.163.25",
+    user: "admin",
+    password: "admin",
+    database: "team2"
+  });
+}
+
+/**
+ * This function search values based on the filter string
+ * @param req
+ * @param res
+ * @param next
+ */
+function search(req, res, next){
+  var filterValue = req.query.search;
+  console.log("Filter String " +filterValue);
+  var con = createConnection();
+  con.connect(function(err) {
+    if (err) throw err;
+    var sql = "SELECT TITLE, DESCRIPTION, LOCATION, EVENT_DATETIME,DURATION, PRICE, IS_PUBLIC, MAX_ATTENDING FROM EVENT WHERE TITLE LIKE '%" + filterValue + "%' OR DESCRIPTION LIKE '%" + filterValue +"%'"
+        + " OR LOCATION LIKE '%" + filterValue +"%'";
+
+    con.query(sql, function (err, result) {
+      con.end();
+      if (err) throw err;
+      /*for (var i = 0; i < result.length; i++) {
+        console.log("EMP ID ::" + result[i].id + "EMP Name ::" + result[i].name);
+      }*/
+      res.searchResult = result;
+      res.filterValue = filterValue;
+      next();
+
+    });
+  });
+
+}
+
+/**
+ * Endpoint for search function
+ */
+app.get('/search', function (req, res) {
+  res.render('search');
+});
+
+/**
+ * Endpoint for insert function
+ */
+app.get('/insert', function(req, res){
+  res.render('insert');
+});
+
+
+/**
+ * service to search events
+ */
+app.get('/api/search', search, (req, res) =>{
+  console.log("search Result Length" + res.searchResult.length);
+  res.render('search',{
+    searchResult : res.searchResult,
+    filterValue : res.filterValue
+  });
+});
+
+
+/**
+ * service to insert events
+ */
+app.post('/api/insert', function(req, res, next){
+  var event = req.body;
+  var con = createConnection();
+  con.connect(function(err) {
+    if (err) throw err;
+    var sql = "INSERT INTO EVENT (TITLE, DESCRIPTION, LOCATION, EVENT_DATETIME,DURATION, PRICE,IS_PUBLIC, MAX_ATTENDING) VALUES ('"+ event.eventTitle+"','"+ event.description+"','"+ event.location+"','"+
+        event.dateTime+"',"+ event.duration+","+ event.price+",'"+ event.isPublic +"',"+event.maxAttending+")";
+
+    con.query(sql, function (err, result) {
+      con.end();
+      if (err) throw err;
+      console.log("Record Inserted!!");
+      next();
+    });
+  });
+  res.render('insert');
+
+});
+
