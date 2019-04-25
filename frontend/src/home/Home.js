bf82c089012aba7c7875150e3054f721f8263cf5
BenLouie <benjaminlouie962@gmail.com>
2019-04-24 22:26:57 -0700

    vertical prototype

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Layout,Menu,Icon} from "antd";
+import { Layout, Menu, Breadcrumb } from 'antd';
 import {Link,HashRouter,Route,Switch} from 'react-router-dom'
 import Admin from '../Admin'
 import Event from '../Event'
@@ -15,77 +15,34 @@ export default class Home extends Component {
         render(){
     return (
     <HashRouter>
-        <Layout style={{minHeight: '100vh'}}>
-            <Sider
-                style={{zIndex: 10000}}
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <div className="logo">
-                </div>
-
-                <Menu onClick={() => {
-                }} mode="vertical" theme={'dark'}>
-                    <Menu.Item key={'Admin'}>
-                        <Link to={'./admin'}>
-                            <Icon type="user"/>
-                            <span>Admin Page</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key={'Event'}>
-                        <Link to={'../Event'}>
-                            <Icon type="form"/>
-                            <span>Event Page</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key={'Login'}>
-                        <Link to={'./Login'}>
-                            <Icon type="form"/>
-                            <span>Login</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key={'Sign Up'}>
-                        <Link to={'./sign up'}>
-                            <Icon type="form"/>
-                            <span>Sign Up</span>
-                        </Link>
-                    </Menu.Item>
-
-                    <Menu.Item key={'ManageEvent'}>
-                        <Link to={'./manage'}>
-                            <Icon type="form"/>
-                            <span>Manage Events</span>
-                        </Link>
-                    </Menu.Item>
-
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
                 </Menu>
-            </Sider>
-
-            <Layout>
-                <Header style={{background: '#fff', padding: 0}}>
-                </Header>
-                <Content>
-                    <div>
-                        <Switch>
-                            <Route  path= "../Admin" component={Admin}/>
-                            <Route  path= "../Event" component={Event}/>
-                        </Switch>
-                    </div>
-                </Content>
-                <Footer style={{textAlign: 'center'}}>
-                    ©{new Date().getFullYear()} SFEVENT
-                </Footer>
-            </Layout>
-        </Layout>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2018 Created by Ant UED
+            </Footer>
+        </Layout>,
+        mountNode
+        );
     </HashRouter>
 
     )
