482fbbbc4c642869fe48257f2088edff9329b25c
Tejal Ghadge <tejalghadge@sfs-wifi-dhcp-10-143-207-227.wifi.sfsu.edu>
2019-04-25 19:27:34 -0700

    update home

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,8 +1,9 @@
 import React,{Component} from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Layout, Menu, Row,Col, Card, Form, Input, Icon, DatePicker, Dropdown,Button} from 'antd';
 import {Link,HashRouter,Route,Switch} from 'react-router-dom'
 import Admin from '../Admin'
 import Event from '../Event'
+
 
 const {Header,Content,Sider,Footer} = Layout;
 
@@ -13,34 +14,94 @@ export default class Home extends Component {
     }
 
         render(){
-    return (
+            const formItemLayout = {
+                labelCol: {
+                    xs: { span: 24 },
+                    sm: { span: 8 },
+                },
+                wrapperCol: {
+                    xs: { span: 24 },
+                    sm: { span: 16 },
+                },
+            };
+
+;
+
+            return (
+
     <HashRouter>
-        <Layout className="layout">
+        <Layout className="layout" style={{height:'100vh'}}>
             <Header>
                 <div className="logo" />
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['2']}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1">Admin</Menu.Item>
-                    <Menu.Item key="2">Event</Menu.Item>
-                    <Menu.Item key="3"><a href="/login">Login</a></Menu.Item>
+                    <Menu.Item key="2" style={{float:'left'}}><a href="/">SFEVENTS</a></Menu.Item>
+
+                    <Menu.Item key="3" style={{float:'right'}}><a href="/login">Login</a></Menu.Item>
+
                 </Menu>
             </Header>
-            <Content style={{ padding: '0 50px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                </Breadcrumb>
-                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
-                Ant Design ©2018 Created by Ant UED
-            </Footer>
-        </Layout>,
-        mountNode
-        );
+            <Layout>
+                    <Row>
+                    <Col>
+                        <Card
+                            hoverable
+                            cover={<img alt="example" src="/events.jpg"  style={{height:'60vh',width:'auto',margin:'auto'}}/>}
+                        >
+                        </Card>
+                    </Col>
+                        <Col>
+                            <Form {...formItemLayout}  style={{margin: 20}} >
+                            <Row gutter={6}>
+                                <Col lg={5} md={12}>
+                                <Form.Item style={{width:'100%'}}>
+                                    <DatePicker style={{width:'100%'}}showTime placeholder="From" required/>
+                                </Form.Item>
+                                </Col>
+                                <Col lg={5} md={12}>
+                                <Form.Item style={{width:'100%'}}>
+                                    <DatePicker style={{width:'100%'}} showTime placeholder="To"required/>
+                                </Form.Item>
+                                </Col>
+
+                                <Col lg={5} md={12}>
+                                <Form.Item style={{width:'100%'}}>
+                                    <Input  style={{width:'100%'}} placeholder="City" />
+                                </Form.Item>
+                                </Col>
+                                <Col lg={5} md={12}>
+                                <Form.Item style={{width:'100%'}}>
+                                    <Input style={{width:'100%'}}  placeholder="Type to Search Event" />
+                                </Form.Item>
+                                </Col>
+                                <Col lg={4} md={12}>
+                                <Form.Item>
+                                    <Button style={{float: 'right'}}
+                                            type="primary"
+                                            htmlType="submit">
+                                        Search
+                                    </Button>
+                                </Form.Item>
+                                </Col>
+                            </Row>
+
+                            </Form>
+                        </Col>
+                    </Row>
+                <Footer>
+                    <div style={{"text-align": "center"}}>
+                    @2019 SFEvents.com <a href="/privacy">privacy Policy</a>
+                    </div>
+                </Footer>
+
+
+
+            </Layout>
+
+        </Layout>
     </HashRouter>
 
     )
