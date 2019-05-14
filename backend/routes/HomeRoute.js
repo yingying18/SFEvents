fc6ebd34f8403b8fa21e7c05ce9800732be583bb
Tejal Ghadge <tejalghadge@Tejals-MacBook-Pro.local>
2019-05-14 00:19:10 -0700

    Changed search API

--- backend/routes/HomeRoute.js
+++ backend/routes/HomeRoute.js
@@ -1,3 +1,9 @@
+const HomeController = require('../controllers/HomeController.js');
+
 module.exports = function(app) {
 
+    app.get('/api/search', function(req, res) {
+
+         HomeController.search(req, res);
+    });
 }
