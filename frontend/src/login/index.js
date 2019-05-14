79e585507a9725425e63ce8ada1677ab1cc73311
Eugene li <eugeneli71@gmail.com>
2019-05-14 00:22:37 -0700

    login

--- frontend/src/login/index.js
+++ frontend/src/login/index.js
@@ -10,14 +10,23 @@ class NormalLoginForm extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
+            console.log(values)
             if (!err) {
-                axios.post('/api/login',values).then(()=>{
+                var request = new XMLHttpRequest();
+                request.open('POST', '/api/login', true);
+                request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+                request.send(JSON.stringify(values));
 
-                }).catch((err)=>{
-                    console.log(err)
-                })
+                // axios.post('/api/login',values).then(()=>{
+                //
+                // }).catch((err)=>{
+                //     console.log(err)
+                // })
             }
         });
+    }
+    componentDidMount() {
+
     }
 
     render() {
@@ -27,33 +36,34 @@ class NormalLoginForm extends React.Component {
                 <center><img src="http://lms.iukl.edu.my/images/icons/register_icon.png" id="icon" alt="" width="200" height="200" /></center>
                 <h1 style={{textAlign: 'center'}}><font size="+10">Login</font></h1>
                <Col lg={8} offset={8}>
-                <Form  onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Email can not be empty!' }],
-                        })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Password can not be empty!' }],
-                        })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                        )}
-                    </Form.Item>
-                    <div style={{textAlign:'center',marginTop:10,marginBottom:20}}>
-                        <Button type="primary" htmlType="submit" style={{width:'100%'}}className="login-form-button">
-                            Log in
-                        </Button>
-                    </div>
-                    <div style={{textAlign:'center'}}>
-                        <a className="login-form-forgot" href="/password/recovery">Forgot password</a>
-                    </div>
-                    <div style={{textAlign:'center',marginTop:15}}>
-                        Don't have an account ?  <a href="/signup">register now!</a>
-                    </div>
-                </Form>
+                   <form action="/api/login" method="post">
+                           <Form.Item>
+                               {getFieldDecorator('username', {
+                                   rules: [{ required: true, message: 'Email can not be empty!' }],
+                               })(
+                                   <Input name={'username'} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                               )}
+                           </Form.Item>
+                           <Form.Item>
+                               {getFieldDecorator('password', {
+                                   rules: [{ required: true, message: 'Password can not be empty!' }],
+                               })(
+                                   <Input name={'password'} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                               )}
+                           </Form.Item>
+                           <div style={{textAlign:'center',marginTop:10,marginBottom:20}}>
+                               <Button type="primary" htmlType="submit" style={{width:'100%'}}className="login-form-button">
+                                   Log in
+                               </Button>
+                           </div>
+                           <div style={{textAlign:'center'}}>
+                               <a className="login-form-forgot" href="/password/recovery">Forgot password</a>
+                           </div>
+                           <div style={{textAlign:'center',marginTop:15}}>
+                               Don't have an account ?  <a href="/signup">register now!</a>
+                           </div>
+                   </form>
+
                </Col>
            </Row>
         );
